0c7c62760c4f30f02acc4c3c8ddd9780aaf0bc0b
aDarkMaker <2261265112@qq.com>
2026-03-25 08:08:33 +0800

    build(web): Vite 代理

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+const backendOrigin = process.env.BACKEND_ORIGIN ?? 'http://localhost:3000';
+
 export default defineConfig({
     srcDir: './src/client',
     integrations: [react()],
     devToolbar: { enabled: false },
+    vite: {
+        server: {
+            proxy: {
+                '/api/': {
+                    target: backendOrigin,
+                    changeOrigin: true,
+                }
+            }
+        }
+    }
 });
